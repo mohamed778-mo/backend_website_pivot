730c4fe8a4246bd5a7bd7115e5b3bca517f24d9b
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-12-15 02:38:25 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,31 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   full_name: { type: String, required: true, trim: true },
-  phone: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+  phone: { type: String, required: false, trim: true },
+  email: { type: String, required: true, trim: true, lowercase: true },
   password: { type: String, required: true },
+  
+  // نوع المستخدم (أدمن لصاحب الموقع / عميل للمشتري)
+  role: { 
+    type: String, 
+    enum: ['admin', 'customer'], 
+    default: 'admin' 
+  },
+
+  // ✅ (للعملاء فقط) الدومين التابعين له
+  domain: { type: String, required: false }, 
+
+  // ✅ (للأدمن فقط) رابط الموقع الخاص به
+  // هذا الحقل سيحمل الـ ID الخاص بالويب سايت اللي الأدمن عمله
+  website: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Website',
+    required: false // غير إلزامي لأن العميل ملوش موقع، والأدمن الجديد لسه معملش موقع
+  }
+
 }, { timestamps: true });
+
+// التأكد من عدم تكرار الإيميل داخل نفس الدومين (للعملاء) أو في النظام ككل (للأدمن)
+UserSchema.index({ email: 1, domain: 1 }, { unique: true });
 
 module.exports = mongoose.model('User', UserSchema);
